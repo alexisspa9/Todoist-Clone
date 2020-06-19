7f4520636ec56d9d04669f53bb732259a49d0019
alexisspa9 <alexisspa9@gmail.com>
2020-06-19 11:18:16 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
 export const App = () => (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello React!</h1>
-      </header>
+      <Header />
+      <Content />
     </div>
 );
 
